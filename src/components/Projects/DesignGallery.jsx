776c92671e82aa3e96284cc6414841ddd3b8881d
FazeelNizam <nizamfazeel@gmail.com>
2026-05-03 23:22:42 +0530

    New component updates

--- src/components/Projects/DesignGallery.jsx
+++ src/components/Projects/DesignGallery.jsx
@@ -10,41 +10,13 @@ import "./DesignGallery.scss";
 gsap.registerPlugin(ScrollTrigger);
 
 const DesignCard = ({ design, onImageClick }) => {
-  const [aspectClass, setAspectClass] = useState("square");
-
-  useEffect(() => {
-    if (!design.image) return;
-
-    const img = new Image();
-    // Handle both string paths and imported image objects
-    const imgSrc = typeof design.image === 'string' ? design.image : (design.image.src || design.image);
-    
-    img.src = imgSrc;
-    img.onload = () => {
-      const ratio = img.naturalWidth / img.naturalHeight;
-      if (ratio > 1.3) {
-        setAspectClass("landscape");
-      } else if (ratio < 0.75) {
-        setAspectClass("portrait");
-      } else {
-        setAspectClass("square");
-      }
-      // Notify ScrollTrigger that layout has shifted due to image load/ratio detection
-      ScrollTrigger.refresh();
-    };
-  }, [design.image]);
-
   return (
     <motion.div
-      layout
       initial={{ opacity: 0, scale: 0.9 }}
       whileInView={{ opacity: 1, scale: 1 }}
       viewport={{ once: true }}
-      transition={{ 
-        duration: 0.5,
-        layout: { duration: 0.6, type: "spring", stiffness: 200, damping: 25 }
-      }}
-      className={`design-card ${aspectClass}`}
+      transition={{ duration: 0.5 }}
+      className={`design-card ${design.aspectClass || "square"}`}
       onClick={() => onImageClick(design)}
     >
       <img
@@ -55,6 +27,8 @@ const DesignCard = ({ design, onImageClick }) => {
       />
       
       <div className="design-overlay">
+        <div className="category-label">{design.category}</div>
+        
         <h3 className="design-title">{design.title}</h3>
         
         {design.tools && (
@@ -87,19 +61,83 @@ const DesignCard = ({ design, onImageClick }) => {
 };
 
 const DesignGallery = ({ designs, onImageClick }) => {
+  const [shuffledDesigns, setShuffledDesigns] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    
+    const shuffle = (array) => {
+      const newArray = [...array];
+      for (let i = newArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+      }
+      return newArray;
+    };
+
+    const loadRatios = async () => {
+      const projectsWithRatios = await Promise.all(
+        designs.map((design) => {
+          return new Promise((resolve) => {
+            const img = new Image();
+            const imgSrc = typeof design.image === 'string' ? design.image : (design.image.src || design.image);
+            img.src = imgSrc;
+            img.onload = () => {
+              const ratio = img.naturalWidth / img.naturalHeight;
+              let aspectClass = "square";
+              if (ratio > 1.3) aspectClass = "landscape";
+              else if (ratio < 0.75) aspectClass = "portrait";
+              resolve({ ...design, aspectClass });
+            };
+            img.onerror = () => resolve({ ...design, aspectClass: "square" });
+          });
+        })
+      );
+
+      setShuffledDesigns(shuffle(projectsWithRatios));
+      setIsLoading(false);
+      
+      setTimeout(() => {
+        ScrollTrigger.refresh();
+      }, 500);
+    };
+
+    loadRatios();
+  }, [designs]);
+
   return (
-    <div className="w-full py-12 px-4 md:px-8">
-      <motion.div layout className="design-gallery-grid">
-        <AnimatePresence>
-          {designs.map((design) => (
-            <DesignCard 
-              key={design.id} 
-              design={design} 
-              onImageClick={onImageClick} 
-            />
-          ))}
-        </AnimatePresence>
-      </motion.div>
+    <div className="w-full py-12 px-4 md:px-8 min-h-[500px] flex items-center justify-center">
+      <AnimatePresence mode="wait">
+        {isLoading ? (
+          <motion.div
+            key="loader"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex flex-col items-center gap-6"
+          >
+            <l-quantum size="45" speed="1.8" color="#9200fa"></l-quantum>
+            <span className="text-xs text-[#9200fa] tracking-[0.3em] uppercase font-bold opacity-80">Curating Gallery</span>
+          </motion.div>
+        ) : (
+          <motion.div 
+            key="grid"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            className="design-gallery-grid w-full"
+          >
+            {shuffledDesigns.map((design) => (
+              <DesignCard 
+                key={design.id} 
+                design={design} 
+                onImageClick={onImageClick} 
+              />
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
--- src/data/designProjects.js
+++ src/data/designProjects.js
@@ -1,69 +1,282 @@
-import Test01 from '../img/projects/test1.jpeg'
-import Test02 from '../img/projects/test2.jpeg'
-import Test03 from '../img/projects/test3.jpeg'
+import Greatings1 from '../img/projects/greetings1.png'
+import Greatings2 from '../img/projects/greetings2.png'
+import Greatings3 from '../img/projects/greetings3.png'
+import Greatings4 from '../img/projects/greetings4.png'
+import Greatings5 from '../img/projects/greetings5.png'
+import Greatings6 from '../img/projects/greetings6.png'
+import Banner1 from '../img/projects/banner1.png'
+import Banner2 from '../img/projects/banner2.png'
+import Banner3 from '../img/projects/banner3.png'
+import Banner4 from '../img/projects/banner4.png'
+import BC1 from '../img/projects/bc1.png'
+import BC2 from '../img/projects/bc2.png'
+import Cashprize from '../img/projects/cashprize.png'
+import Invitation1 from '../img/projects/invitation1.png'
+import Brochure1 from '../img/projects/brochure1.png'
+import Brochure2 from '../img/projects/brochure2.png'
+import Thanking1 from '../img/projects/thanking1.png'
+import Flyer1 from '../img/projects/flyer1.png'
+import Flyer2 from '../img/projects/flyer2.png'
+import Flyer3 from '../img/projects/flyer3.png'
+import Flyer4 from '../img/projects/flyer4.png'
+import Flyer5 from '../img/projects/flyer5.png'
+import Flyer6 from '../img/projects/flyer6.png'
+import Flyer7 from '../img/projects/flyer7.png'
+import Flyer8 from '../img/projects/flyer8.png'
+import Flyer9 from '../img/projects/flyer9.png'
+import Flyer10 from '../img/projects/flyer10.png'
+import Flyer11 from '../img/projects/flyer11.png'
 
 export const designProjects = [
   {
     id: 1,
     type : "design",
-    title: "Banners",
-    image: Test01,
+    category : "Banner",
+    title: "Career Fair Banner",
+    image: Banner1,
     behanceUrl: "https://www.behance.net/gallery/241490155/Banners",
     tools: ["Adobe Photoshop", "Adobe Illustrator", "Figma"],
   },
   {
     id: 2,
     type : "design",
-    title: "Business Cards",
-    image: Test02,
-    behanceUrl: "https://www.behance.net/gallery/241489869/Business-Cards",
-    tools: ["Adobe Illustrator", "Figma"],
+    category : "Banner",
+    title: "Business Banners",
+    image: Banner2,
+    behanceUrl: "https://www.behance.net/gallery/241490155/Banners",
+    tools: ["Adobe Photoshop", "Adobe Illustrator", "Figma"],
   },
   {
     id: 3,
     type : "design",
-    title: "Invitations",
-    image: Test03,
-    behanceUrl: "https://www.behance.net/gallery/194870365/Invitations",
-    tools: ["Adobe Photoshop", "Canva"],
+    category : "Banner",
+    title: "Poson Dansal Banner",
+    image: Banner3,
+    behanceUrl: "https://www.behance.net/gallery/241490155/Banners",
+    tools: ["Adobe Photoshop", "Adobe Illustrator", "Figma"],
   },
   {
     id: 4,
     type : "design",
-    title: "Cash Prize",
-    image: Test01,
-    behanceUrl: "https://www.behance.net/gallery/194869791/Cash-Prize",
-    tools: ["Adobe Illustrator", "Adobe Photoshop"],
+    category : "Banner",
+    title: "Vesak Dansal Banner",
+    image: Banner4,
+    behanceUrl: "https://www.behance.net/gallery/241490155/Banners",
+    tools: ["Adobe Photoshop", "Adobe Illustrator", "Figma"],
   },
   {
     id: 5,
     type : "design",
-    title: "Brochure",
-    image: Test02,
-    behanceUrl: "https://www.behance.net/gallery/194869807/Brochure",
+    category : "Business Card",
+    title: "Hand Made By Aura - Business Card",
+    image: BC1,
+    behanceUrl: "https://www.behance.net/gallery/241489869/Business-Cards",
     tools: ["Adobe Illustrator", "Figma"],
   },
   {
     id: 6,
     type : "design",
-    title: "Thanking Card",
-    image: Test03,
-    behanceUrl: "https://www.behance.net/gallery/194869823/Thanking-Card",
-    tools: ["Adobe Photoshop"],
+    category : "Business Card",
+    title: "Poooh - Business Card",
+    image: BC2,
+    behanceUrl: "https://www.behance.net/gallery/241489869/Business-Cards",
+    tools: ["Adobe Illustrator", "Figma"],
   },
   {
     id: 7,
     type : "design",
-    title: "Greetings",
-    image: Test01,
-    behanceUrl: "https://www.behance.net/gallery/194869551/Greetings",
-    tools: ["Adobe Illustrator", "Canva"],
+    category : "Cash Prize",
+    title: "ENOSPIRE OUSL - Cash Prize",
+    image: Cashprize,
+    behanceUrl: "https://www.behance.net/gallery/194869791/Cash-Prize",
+    tools: ["Adobe Illustrator", "Adobe Photoshop"],
   },
   {
     id: 8,
     type : "design",
+    category : "Invitation",
+    title: "SVP 42nd Anniversery - Invitation",
+    image: Invitation1,
+    behanceUrl: "https://www.behance.net/gallery/194870365/Invitations",
+    tools: ["Adobe Photoshop", "Canva"],
+  },
+  {
+    id: 9,
+    type : "design",
+    category : "Brochure",
+    title: "British Council YCA - Brochure",
+    image: Brochure1,
+    behanceUrl: "https://www.behance.net/gallery/194869807/Brochure",
+    tools: ["Adobe Illustrator", "Figma"],
+  },
+  {
+    id: 10,
+    type : "design",
+    category : "Brochure",
+    title: "Scoops of Campechiana - Brochure",
+    image: Brochure2,
+    behanceUrl: "https://www.behance.net/gallery/194869807/Brochure",
+    tools: ["Adobe Illustrator", "Figma"],
+  },
+  {
+    id: 11,
+    type : "design",
+    category : "Thanking Card",
+    title: "TalentLyft 2025 - Thanking Card",
+    image: Thanking1,
+    behanceUrl: "https://www.behance.net/gallery/194869823/Thanking-Card",
+    tools: ["Adobe Photoshop"],
+  },
+  {
+    id: 12,
+    type : "design",
+    category : "Greetings",
+    title: "Christmas Greeting",
+    image: Greatings1,
+    behanceUrl: "https://www.behance.net/gallery/194869551/Greetings",
+    tools: ["Adobe Illustrator", "Canva"],
+  },
+  {
+    id: 13,
+    type : "design",
+    category : "Greetings",
+    title: "Christmas Greeting",
+    image: Greatings2,
+    behanceUrl: "https://www.behance.net/gallery/194869551/Greetings",
+    tools: ["Adobe Illustrator", "Canva"],
+  },
+  {
+    id: 14,
+    type : "design",
+    category : "Greetings",
+    title: "New Year Greeting",
+    image: Greatings3,
+    behanceUrl: "https://www.behance.net/gallery/194869551/Greetings",
+    tools: ["Adobe Illustrator", "Canva"],
+  },
+  {
+    id: 15,
+    type : "design",
+    category : "Greetings",
+    title: "New Year Greeting",
+    image: Greatings4,
+    behanceUrl: "https://www.behance.net/gallery/194869551/Greetings",
+    tools: ["Adobe Illustrator", "Canva"],
+  },
+  {
+    id: 16,
+    type : "design",
+    category : "Greetings",
+    title: "Poson Greeting",
+    image: Greatings5,
+    behanceUrl: "https://www.behance.net/gallery/194869551/Greetings",
+    tools: ["Adobe Illustrator", "Canva"],
+  },
+  {
+    id: 17,
+    type : "design",
+    category : "Greetings",
+    title: "Vesak Greeting",
+    image: Greatings6,
+    behanceUrl: "https://www.behance.net/gallery/194869551/Greetings",
+    tools: ["Adobe Illustrator", "Canva"],
+  },
+  {
+    id: 18,
+    type : "design",
+    category : "Flyer",
     title: "Flyers",
-    image: Test02,
+    image: Flyer1,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 19,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer2,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 20,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer3,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 21,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer4,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 22,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer5,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 23,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer6,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 24,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer7,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 25,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer8,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 26,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer9,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 27,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer10,
+    behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
+    tools: ["Adobe Photoshop", "Adobe Illustrator"],
+  },
+  {
+    id: 28,
+    type : "design",
+    category : "Flyer",
+    title: "Flyers",
+    image: Flyer11,
     behanceUrl: "https://www.behance.net/gallery/194867505/Flyers",
     tools: ["Adobe Photoshop", "Adobe Illustrator"],
   }
